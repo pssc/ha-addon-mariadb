667f2f2c0d9ec16546748e1f4c00655198a79076
pssc <phillip.camp+git@gmail.com>
2020-06-08 10:30:50 +0100

    Tidy container Timeout and provide and env var with the setting

--- mariadbp/rootfs/etc/cont-init.d/03-s6-env.sh
+++ mariadbp/rootfs/etc/cont-init.d/03-s6-env.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/with-contenv bash
 # ==============================================================================
-# Community Hass.io Add-ons: 
+# Community Hass.io Add-ons: mariadb+
 # ==============================================================================
 
-echo -n 179000 >/run/s6/container_environment/S6_SERVICES_GRACETIME
+# We are setup for three minutes
+# FIXME set from Docker build?
+HAA_TIMEOUT=${HAA_TIMEOUT:-"180"}
+echo -n ${HAA_TIMEOUT}  > /run/s6/container_environment/HAA_TIMEOUT
+# 99% of our limit
+echo -n $(($HAA_TIMEOUT*990)) >/run/s6/container_environment/S6_SERVICES_GRACETIME
 #echo -n 100000 >/run/s6/env-stage2/S6_SERVICES_GRACETIME
 #echo -n 100000 >/run/s6/env-stage3/S6_SERVICES_GRACETIME
 #echo -n 100000 >/run/s6/container_environment/S6_KILL_FINISH_MAXTIME
-
