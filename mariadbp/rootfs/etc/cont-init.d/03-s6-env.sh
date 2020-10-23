0e1e000a09cee6f4cc1f234e5c753c3f88ad8c86
pssc <phillip.camp+git@gmail.com>
2020-10-23 11:49:02 +0100

    Allow for max timout for continer shutdown in config.json 5m

--- mariadbp/rootfs/etc/cont-init.d/03-s6-env.sh
+++ mariadbp/rootfs/etc/cont-init.d/03-s6-env.sh
@@ -3,12 +3,10 @@
 # Community Hass.io Add-ons: mariadb+
 # ==============================================================================
 
-# We are setup for three minutes
+# We are setup for Five minutes the max
 # FIXME set from Docker build?
-HAA_TIMEOUT=${HAA_TIMEOUT:-"180"}
+HAA_TIMEOUT=${HAA_TIMEOUT:-"300"}
 echo -n ${HAA_TIMEOUT}  > /run/s6/container_environment/HAA_TIMEOUT
 # 99% of our limit
 echo -n $(($HAA_TIMEOUT*990)) >/run/s6/container_environment/S6_SERVICES_GRACETIME
-#echo -n 100000 >/run/s6/env-stage2/S6_SERVICES_GRACETIME
-#echo -n 100000 >/run/s6/env-stage3/S6_SERVICES_GRACETIME
-#echo -n 100000 >/run/s6/container_environment/S6_KILL_FINISH_MAXTIME
+echo -n $(($HAA_TIMEOUT*990)) >/run/s6/container_environment/S6_KILL_FINISH_MAXTIME
